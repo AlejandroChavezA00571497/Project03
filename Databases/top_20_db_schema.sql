2c7e240947d5b59347499db03962b9643d218039
AlejandroChavezA00571497 <alejandrochavez108@outlook.com>
2023-09-21 21:19:04 -0600

    Actualizando SQL Schema

--- Databases/top_20_db_schema.sql
+++ Databases/top_20_db_schema.sql
@@ -1,33 +1,24 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/6Nt9UH
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "top_20_companies" (
-    "id" int   NOT NULL,
-    "timestamp" int   NOT NULL,
-    "company" varchar   NOT NULL,
-    "level" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
-    "totalyearlycompensation" int   NOT NULL,
-    "location" varchar   NOT NULL,
-    "yearsofexperience" float   NOT NULL,
-    "yearsatcompany" float   NOT NULL,
-    "tag" varchar   NOT NULL,
-    "gender" varchar   NOT NULL,
-    "cityid" int   NOT NULL,
-    "dmaid" float   NOT NULL,
-    "rownumber" int   NOT NULL,
-    "mastersdegree" int   NOT NULL,
-    "bachelorsdegree" int   NOT NULL,
-    "doctoratedegree" int   NOT NULL,
-    "raceasian" int   NOT NULL,
-    "racewhite" int   NOT NULL,
-    "racetwoormore" int   NOT NULL,
-    "raceblack" int   NOT NULL,
-    "racehispanic" int   NOT NULL,
-    CONSTRAINT "pk_top_20_companies" PRIMARY KEY (
-        "id"
-     )
-);
-
+﻿CREATE TABLE top_20_companies (
+    id INT  NOT NULL ,
+    timestamp DATE  NOT NULL ,
+    company VARCHAR  NOT NULL ,
+    level VARCHAR,
+    title VARCHAR  NOT NULL ,
+    totalyearlycompensation INT  NOT NULL ,
+    location VARCHAR  NOT NULL ,
+    yearsofexperience float  NOT NULL ,
+    yearsatcompany float  NOT NULL ,
+    tag VARCHAR,
+    gender VARCHAR,
+    cityid int  NOT NULL ,
+    dmaid float  NOT NULL ,
+    rownumber int  NOT NULL ,
+    mastersdegree BOOLEAN  NOT NULL ,
+    bachelorsdegree BOOLEAN  NOT NULL ,
+    doctoratedegree BOOLEAN  NOT NULL ,
+    raceasian BOOLEAN  NOT NULL ,
+    racewhite BOOLEAN  NOT NULL ,
+    racetwoormore BOOLEAN  NOT NULL ,
+    raceblack BOOLEAN  NOT NULL ,
+    racehispanic BOOLEAN  NOT NULL
+)
