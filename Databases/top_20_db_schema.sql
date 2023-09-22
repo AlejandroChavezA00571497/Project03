c743c55f71bfdd6934f734347d53afd52710fdfe
AlejandroChavezA00571497 <alejandrochavez108@outlook.com>
2023-09-21 20:40:34 -0600

    Schema de DB SQL

---
+++ Databases/top_20_db_schema.sql
@@ -0,0 +1,38 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/6Nt9UH
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [top_20_companies] (
+    [id] int  NOT NULL ,
+    [timestamp] int  NOT NULL ,
+    [company] string  NOT NULL ,
+    [level] string  NOT NULL ,
+    [title] string  NOT NULL ,
+    [totalyearlycompensation] int  NOT NULL ,
+    [location] string  NOT NULL ,
+    [yearsofexperience] float  NOT NULL ,
+    [yearsatcompany] float  NOT NULL ,
+    [tag] string  NOT NULL ,
+    [gender] string  NOT NULL ,
+    [cityid] int  NOT NULL ,
+    [dmaid] float  NOT NULL ,
+    [rownumber] int  NOT NULL ,
+    [mastersdegree] int  NOT NULL ,
+    [bachelorsdegree] int  NOT NULL ,
+    [doctoratedegree] int  NOT NULL ,
+    [raceasian] int  NOT NULL ,
+    [racewhite] int  NOT NULL ,
+    [racetwoormore] int  NOT NULL ,
+    [raceblack] int  NOT NULL ,
+    [racehispanic] int  NOT NULL ,
+    CONSTRAINT [PK_top_20_companies] PRIMARY KEY CLUSTERED (
+        [id] ASC
+    )
+)
+
+COMMIT TRANSACTION QUICKDBD
