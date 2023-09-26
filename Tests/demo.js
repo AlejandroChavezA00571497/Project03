1a6cf027d8fcc5c868857c82d6c1c4cd864ba09c
AlejandroChavezA00571497 <alejandrochavez108@outlook.com>
2023-09-25 19:54:15 -0600

    Añadiendo estructura básica al html

--- Tests/demo.js
+++ Tests/demo.js
@@ -1,24 +1,8 @@
-/*// Get the Roadster endpoint
-const roadster = "https://api.spacexdata.com/v4/roadster";
-
-// Fetch the JSON data and console log it
-d3.json(roadster).then(function(data) {
-  console.log(data);
-});
-
-// Get the capsules endpoint
-const capsules = "https://api.spacexdata.com/v4/capsules";
-
-// Fetch the JSON data and console log it
-d3.json(capsules).then(function(data) {
-  console.log(data);
-});
-*/
-
+// Comando en terminal donde esté el html:
+// python -m http.server 8001
 
 d3.json("http://127.0.0.1:5000/api/v1.0/company").then(function(data) {
   console.log(data);
 });
 
 
-// python -m http.server
