0793ebbd674592347ec20f1b1b2f99cd635eb0ab
AlejandroChavezA00571497 <alejandrochavez108@outlook.com>
2023-09-25 22:19:01 -0600

    Algunos cambios al js

--- Tests/demo.js
+++ Tests/demo.js
@@ -7,12 +7,60 @@ const baseUrl = "http://127.0.0.1:5000/api/v1.0/alldata"
 
 
 // Console log ejemplo con la información de company
-d3.json(baseUrl).then(function(data) {
-  console.log(data.company);
+d3.json(baseUrl).then(function(jsonData) {
+  console.log(jsonData);
 });
 
-d3.json(baseUrl).then(function(data){
-  company = data[0];
+//Reading the data, extracting the first company's data and using it to run the inital bar chart function
+d3.json(baseUrl).then(function(jsonData){
+  let firstCompany = jsonData[0]["company"];
+  console.log(firstCompany)
+  initialChart(firstCompany)
 });
 
-console.log(company)
+
+// Define function for the initial chart
+function initialChart(firstCompany){
+  d3.json(baseUrl).then(function(jsonData){
+      let subjectSelector = d3.select("#selDataset");
+      let companies = jsonData.company;
+      companies.forEach(function(company){
+          subjectSelector.append("option").text(company);
+      })
+  
+      barChart(firstCompany);
+
+  })
+}
+
+
+//Define function for building the barCharts
+function barChart(firstCompany){
+  d3.json(baseUrl).then(function(jsonData){
+      let companyData = jsonData.samples.filter(x => x.id == firstCompany);
+
+
+      let barChartData = [{
+          x : companyData[0].company,
+          y : companyData[0].totalyearlycompensation,
+          type : "bar",
+          orientation : "h",
+          text : companyData[0].totalyearlycompensation
+      }];
+
+      let barChartLayout = {
+          height : 500,
+          width : 600,
+          yaxis : {type : "category"}
+      }
+
+      Plotly.newPlot("bar", barChartData, barChartLayout)
+  })
+};
+
+
+//Define the optionChanged function present in the HTML, in order to actually change the visualizations according to the selected SubjectID
+
+function optionChanged(firstCompany){
+  barChart(firstCompany);
+};
